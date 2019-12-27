847d4ea017ce801c4aef6a33b799983eed9ad9fb
TFprimeWJ <jw.tfprime@gmail.com>
2019-12-27 10:08:59 +0800

    110

---
+++ 110_Balanced_Binary_Tree/BalancedBinaryTree_DFS.java
@@ -0,0 +1,23 @@
+class BalancedBinaryTree_DFS {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode (int _val) {
+            this.val = _val;
+        }
+    }
+
+    public boolean isBalanced (TreeNode root) {
+        return depth(root) != -1;
+    }
+
+    private int depth (TreeNode root) {
+        if (root == null) return 0;
+        int left = depth(root.left);
+        if (left == -1) return -1;
+        int right = depth(root.right);
+        if (right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+}

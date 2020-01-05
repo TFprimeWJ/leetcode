7a9e133444c6afb9d3e3d9f14a8b97f263c61f3a
TFprimeWJ <jw.tfprime@gmail.com>
2020-01-06 03:16:59 +0800

    add 27

--- 27_Remove_Element/RemoveElement.java
+++ 27_Remove_Element/RemoveElement.java
@@ -4,8 +4,7 @@ class RemoveElement {
         int length = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[length] = nums[i];
-                length++;
+                nums[length++] = nums[i];
             }
         }
         return length + 1;
